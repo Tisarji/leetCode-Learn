56bc186d134edc587e48b5909b2d4e3a7c277297
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-05-09 16:20:21 +0700

    update

--- swap-nodes-in-pairs/swap-nodes-in-pairs.c
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.c
@@ -1,36 +1,69 @@
 /**
-* Subject in Swap-Nodes-In-Pairs
-* 
-* Given a linked list, swap every two adjacent nodes and return its head. 
-* You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
-*/
+ * Subject in Swap-Nodes-In-Pairs
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ * You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 // Definition for singly-linked list.
-struct	ListNode {
+struct ListNode
+{
 	int val;
 	struct ListNode *next;
 };
 
-void	swap_node(struct ListNode *node1, struct ListNode *node2)
+// Function to create a new node
+struct ListNode*	createNode(int value)
 {
-	int	temp;
-	temp = node1->val;
-	node1->val = node2->val;
-	node2->val = temp;
+	struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+	newNode->val = value;
+	newNode->next = NULL;
+	return (newNode);
 }
 
-struct ListNode*	swapPairs(struct ListNode* head)
+struct ListNode	*swapPairs(struct ListNode *head)
 {
-	if (!head || !head->next)
+	if (head == NULL || head->next == NULL)
 		return (head);
-	
+
+	struct ListNode *newHead = head->next;
+	struct ListNode *nextPair = newHead->next;
+	newHead->next = head;
+	head->next = swapPairs(nextPair);
+
+	return (newHead);
 }
 
-int	main(void)
+void	print_list(struct ListNode *head)
 {
-	printf("\n");
-	return (0);
+	while (head != NULL)
+	{
+		printf("%d", head->val);
+		head = head->next;
+	}
+	// printf("\n");
 }
+/**
+*	int main(void)
+*	{
+*		struct ListNode *head = NULL;
+*		head = createNode(1);
+*		head->next = createNode(2);
+*		head->next->next = createNode(3);
+*		head->next->next->next = createNode(4);
+*	
+*		printf("Original: ");
+*		print_list(head);
+*		printf("\n");
+*	
+*		head = swapPairs(head);
+*		printf("Swap Pairs: ");
+*		print_list(head);
+*		printf("\n");
+*	
+*		return (0);
+*	}
+*/
