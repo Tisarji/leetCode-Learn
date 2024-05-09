bba53c09e54273ec9090f9f833327d5bd01c0528
Jirasit <48820052+TONG2K@users.noreply.github.com>
2024-05-09 16:44:08 +0700

    update

---
+++ compile.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BOLD_GREEN='\033[1;32m'
+PINK='\033[0;95m'
+NORMAL='\033[0m'
+
+if [ $# -eq 0 ]; then
+  echo -e "${GREEN}Please provide a C source file path and name as an argument.${NORMAL}"
+  echo -e "\t ${PINK}Usage -> ./compiler.sh [path/to/file.c] ${NORMAL}"
+  exit 1
+fi
+
+full_path="$1"
+
+if [ ! -f "$full_path" ]; then
+  echo -e "${RED}Error:${NORMAL} File '$full_path' does not exist."
+  exit 1
+fi
+
+filename="${full_path%.*}"
+gcc -o "$filename" "$full_path"
+
+if [ $? -ne 0 ]; then
+  echo -e "${RED}Error:${NORMAL} Compilation failed for '$full_path'."
+  exit 1
+fi
+
+echo
+echo -e "${BOLD_GREEN}** Compilation successful. **"
+echo "Executable created as '$filename'."
--- swap-nodes-in-pairs/swap-nodes-in-pairs.c
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.c
@@ -1,5 +1,5 @@
 /**
- * Subject in Swap-Nodes-In-Pairs
+ * Subject: in Swap-Nodes-In-Pairs
  *
  * Given a linked list, swap every two adjacent nodes and return its head.
  * You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
@@ -46,24 +46,23 @@ void	print_list(struct ListNode *head)
 	}
 	// printf("\n");
 }
-/**
-*	int main(void)
-*	{
-*		struct ListNode *head = NULL;
-*		head = createNode(1);
-*		head->next = createNode(2);
-*		head->next->next = createNode(3);
-*		head->next->next->next = createNode(4);
-*	
-*		printf("Original: ");
-*		print_list(head);
-*		printf("\n");
-*	
-*		head = swapPairs(head);
-*		printf("Swap Pairs: ");
-*		print_list(head);
-*		printf("\n");
-*	
-*		return (0);
-*	}
-*/
+
+	int main(void)
+	{
+		struct ListNode *head = NULL;
+		head = createNode(1);
+		head->next = createNode(2);
+		head->next->next = createNode(3);
+		head->next->next->next = createNode(4);
+	
+		printf("Original: ");
+		print_list(head);
+		printf("\n");
+	
+		head = swapPairs(head);
+		printf("Swap Pairs: ");
+		print_list(head);
+		printf("\n");
+	
+		return (0);
+	}
